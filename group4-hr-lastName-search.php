cb66dfcc48d272582defd3bfeee30f180b4f0d25
mrvertig0 <57294639+mrvertig0@users.noreply.github.com>
2022-04-26 20:37:04 -0400

    Add files via upload

---
+++ group4-hr-lastName-search.php
@@ -0,0 +1,71 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+		<!--Name:
+				filename: group4-hr-lastName-search.php
+				Blackboard Username: zrhines
+				Class Section: CTI.110.0004
+				Purpose: Group Project
+		-->
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>HR Employee Search</title>
+	<link href="group4-hr-style.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+<div class="container">
+	<header>
+		<h1>HR Employee Search Results</h1>
+	</header>
+<main>
+<?php
+include('db-connection.php');
+$connect = mysqli_connect($server, $user, $pw, $db);
+if (!$connect) { 
+    print("ERROR: Cannot connect to database $db on server $server using username $user ("
+        . mysqli_connect_errno() . ", " . mysqli_connect_error() . ")");
+}
+$lastName = $_POST['lastName'];
+$query = "select * from personnel;";
+
+$result = mysqli_query($connect, $query);
+if (!$result) {
+    print("Could not successfully run query. Press back to return."
+        . mysqli_error($connect) );
+}
+
+$query = "select * from personnel where lastName = '$lastName' order by lastName;";
+$result = mysqli_query($connect, $query);
+if (!$result) {
+    print("Could not successfully run query. Press back to return."
+        . mysqli_error($connect) );
+}
+if (mysqli_num_rows($result) == 0) { 
+   print("No records found with query $lastName");
+} else {
+    echo "<table>";
+    echo "<caption>Employees</caption>";
+    echo "<tr><th>Employee ID</th><th colspan=2>Name</th><th>Job Code</th><th>Job Title</th><th>Salary</th></tr>";
+    while ($row = mysqli_fetch_assoc($result)) {
+        $empID = $row['empID'];
+		$firstName = $row['firstName'];
+        $lastName = $row['lastName'];
+		$jobCode = $row['jobCode'];
+		$jobTitle = $row['jobTitle'];
+		$salary = $row['salary'];
+        echo "<tr><td>$empID</td><td>$firstName</td><td>$lastName</td><td>$jobCode</td><td>$jobTitle</td><td>$salary</td></tr>";
+    }
+    echo "</table>";
+}
+
+
+mysqli_close($connect);
+?>
+</main>
+<footer>
+<a href="group4-hr-emp-search.html">Return to Employee Search</a>
+</footer>
+</body>
+</html>
+
+
