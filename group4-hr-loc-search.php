2c44770dad89180e8e132e794b031667e9364239
wilsnatq <104107618+wilsnatq@users.noreply.github.com>
2022-04-29 05:48:04 -0400

    Small fixups for department/location search

--- group4-hr-loc-search.php
+++ group4-hr-loc-search.php
@@ -9,7 +9,7 @@
 		-->
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>HR Location Search</title>
+	<title>Location Search Results</title>
 	<link href="group4-hr-style.css" type="text/css" rel="stylesheet" />
 </head>
 <body>
@@ -45,13 +45,14 @@ if (mysqli_num_rows($result) == 0) {
     $state = $row['state_province'];
     $postal_code = $row['postal_code'];
     $location_id = $row['location_id'];
-    echo "Location: $street_address, <b>$city</b>";
+    echo "<h2>$street_address, <b>$city</b>";
     if ($state != null) {
         echo ", $state";
     }
     if ($postal_code != null) {
         echo " $postal_code";
     }
+    echo "</h2>";
 
     $query = "select * from departments where location_id = '$location_id';";
     $result = mysqli_query($connect, $query);
@@ -63,17 +64,19 @@ if (mysqli_num_rows($result) == 0) {
        print("No records found with query $query");
     } else {
         echo "<table>";
-        echo "<caption>Employees in $city by department</caption>";
+        echo "<caption>Employees in $city by Department</caption>";
         while ($row = mysqli_fetch_assoc($result)) {
             $dept = $row['department_name'];
             $dept_id = $row['department_id'];
-            echo "<tr><th colspan=2>$dept</th>/<tr>";
+            echo "<tr><th colspan=4>$dept</th></tr>";
             $query = "select * from employees where department_id = '$dept_id' order by last_name;";
             $result2 = mysqli_query($connect, $query);
             while ($row2 = mysqli_fetch_assoc($result2)) {
                 $first = $row2['first_name'];
                 $last = $row2['last_name'];
-                echo "<tr><td>$first</<td><td>$last</td></tr>";
+                $email = $row2['email'];
+                $phone = $row2['phone_number'];
+                echo "<tr><td>$first</td><td>$last</td><td>$email</td><td>$phone</td></tr>";
             }
         }
         echo "</table>";
