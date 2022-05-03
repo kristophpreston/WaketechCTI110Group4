3141268daf8f5fe119083f0ae116db5f37fbfd71
mrvertig0 <57294639+mrvertig0@users.noreply.github.com>
2022-05-03 18:18:33 -0400

    Update group4-hr-jobTitle-search.php
    used nathan's php work in emp search to replicate for jobTitle search. Looked good locally.

--- group4-hr-jobTitle-search.php
+++ group4-hr-jobTitle-search.php
@@ -1,16 +1,21 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
 		<!--Name:
-				filename: group4-hr-jobTitle-search.php
+				filename: group4-hr-lastName-search.php
 				Blackboard Username: zrhines
 				Class Section: CTI.110.0004
 				Purpose: Group Project
 		-->
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>HR Employee Search</title>
+	<title>HR Job Title Search Results</title>
 	<link href="group4-hr-style.css" type="text/css" rel="stylesheet" />
+  <style>
+    body {
+      width: unset;
+    }
+  </style>
 </head>
 <body>
 <div class="container">
@@ -25,8 +30,16 @@ if (!$connect) {
     print("ERROR: Cannot connect to database $db on server $server using username $user ("
         . mysqli_connect_errno() . ", " . mysqli_connect_error() . ")");
 }
+
+$where = ";";
 $jobTitle = $_POST['jobTitle'];
-$query = "select * from employees;";
+if ($jobTitle) {
+  $where = " where job_title = $jobTitle;";
+}
+
+$query = "select * from employees join jobs "
+       . "on employees.job_id = jobs.job_id "
+       . $where;
 
 $result = mysqli_query($connect, $query);
 if (!$result) {
@@ -34,26 +47,46 @@ if (!$result) {
         . mysqli_error($connect) );
 }
 
-$query = "select * from employees where jobTitle = '$jobTitle' order by jobTitle;";
-$result = mysqli_query($connect, $query);
-if (!$result) {
-    print("Could not successfully run query. Press back to return."
-        . mysqli_error($connect) );
-}
 if (mysqli_num_rows($result) == 0) { 
-   print("No records found with query $query");
+   print("No records found with query $jobTitle");
 } else {
     echo "<table>";
-    echo "<caption>Employees</caption>";
-	echo "<tr><th>Employee ID</th><th colspan=2>Name</th><th>Job Code</th><th>Job Title</th><th>Salary</th></tr>";
+    echo "<tr><th>Employee ID</th><th colspan=2>Name</th><th>Email</th><th>Phone</th><th>Job Code</th><th>Job Title</th><th>Salary</th><th>Hire Date</th><th>Department</th><th>Manager</th></tr>";
     while ($row = mysqli_fetch_assoc($result)) {
-        $empID = $row['empID'];
-		$firstName = $row['firstName'];
-        $lastName = $row['lastName'];
-		$jobCode = $row['jobCode'];
-		$jobTitle = $row['jobTitle'];
-		$salary = $row['salary'];
-        echo "<tr><td>$empID</td><td>$firstName</td><td>$lastName</td><td>$jobCode</td><td>$jobTitle</td><td>$salary</td></tr>";
+      $empID = $row['employee_id'];
+	  $firstName = $row['first_name'];
+      $lastName = $row['last_name'];
+      $email = $row['email'];
+      $phone_number = $row['phone_number'];
+      $hire_date = $row['hire_date'];
+      $manager_id = $row['manager_id'];
+      $department_id = $row['department_id'];        
+	  $jobCode = $row['job_id'];
+	  $jobTitle = $row['job_title'];
+	  $salary = $row['salary'];
+
+      // lookup department name
+      $query2 = "select department_name from departments where department_id = $department_id;";
+      $result2 = mysqli_query($connect, $query2);
+      if ($result2 && (mysqli_num_rows($result2) > 0)) {
+        $row2 = mysqli_fetch_assoc($result2);
+        $department = $row2['department_name'];    
+      } else {
+        $department = "error: " . mysqli_error();
+      }
+      
+      // lookup manager name
+      $query3 = "select first_name,last_name from employees where employee_id = $manager_id;";
+      $result3 = mysqli_query($connect, $query3);
+      if ($result3 && (mysqli_num_rows($result3) > 0)) {
+        $row3 = mysqli_fetch_assoc($result3);
+        $manager = $row3['last_name'];    
+      } else {
+        $manager = "error: " . mysqli_error();
+      }
+      if (!$manager_id) { $manager = "N/A"; }
+      echo "<tr><td>$empID</td><td>$firstName</td><td>$lastName</td><td>$email</td><td>$phone_number</td><td>$jobCode</td><td>$jobTitle</td><td>$" 
+           . number_format($salary, 2) . "</td><td>$hire_date</td><td>$department</td><td>$manager</td></tr>";
     }
     echo "</table>";
 }
@@ -63,7 +96,7 @@ mysqli_close($connect);
 ?>
 </main>
 <footer>
-<a href="group4-hr-job-title.html">Return to Employee Search</a>
+<a href="group4-hr-emp-search.html">Return to Employee Search</a>
 </footer>
 </body>
 </html>
