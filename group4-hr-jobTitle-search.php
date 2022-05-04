a357e85f420ac40c94d884a2e7cdcdbe34195b6d
wilsnatq <104107618+wilsnatq@users.noreply.github.com>
2022-05-04 15:16:44 -0400

    Add list of employees and average salary

--- group4-hr-jobTitle-search.php
+++ group4-hr-jobTitle-search.php
@@ -1,29 +1,33 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html lang="en">
 <head>
 		<!--Name:
-				filename: group4-hr-lastName-search.php
-				Blackboard Username: zrhines
+				filename: group4-hr-job-title.php
+				Blackboard Username: nswilson
 				Class Section: CTI.110.0004
-				Purpose: Group Project
+				Purpose: Group Project Job Title Search
 		-->
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>HR Job Title Search Results</title>
+	<title>Job Title Search Results</title>
 	<link href="group4-hr-style.css" type="text/css" rel="stylesheet" />
   <style>
-    body {
-      width: unset;
+    .center {
+      text-align: center;
     }
   </style>
 </head>
 <body>
 <div class="container">
-	<header>
-		<h1>HR Job Title Search Results</h1>
-	</header>
-<main>
 <?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+$job = $_POST['jobTitle'];
+echo "<header>
+<h1>Job Title Search Results for <i>$job</i></h1>
+</header>
+<main>";
+
 include('db-connection.php');
 $connect = mysqli_connect($server, $user, $pw, $db);
 if (!$connect) { 
@@ -31,73 +35,58 @@ if (!$connect) {
         . mysqli_connect_errno() . ", " . mysqli_connect_error() . ")");
 }
 
-$where = ";";
-$jobTitle = $_POST['jobTitle'];
-if ($jobTitle) {
-  $where = " where job_title = $jobTitle;";
-}
-
-$query = "select * from employees join jobs "
-       . "on employees.job_id = jobs.job_id "
-       . $where;
-
+// first find all job titles that match the search string
+$query = "select * from jobs where job_title like '%$job%';";
 $result = mysqli_query($connect, $query);
 if (!$result) {
     print("Could not successfully run query. Press back to return."
         . mysqli_error($connect) );
 }
-
 if (mysqli_num_rows($result) == 0) { 
-   print("No records found.");
+   print("No records found with query $query");
 } else {
-    echo "<table>";
-    echo "<tr><th>Employee ID</th><th colspan=2>Name</th><th>Email</th><th>Phone</th><th>Job Code</th><th>Job Title</th><th>Salary</th><th>Hire Date</th><th>Department</th><th>Manager</th></tr>";
-    while ($row = mysqli_fetch_assoc($result)) {
-      $empID = $row['employee_id'];
-	  $firstName = $row['first_name'];
-      $lastName = $row['last_name'];
-      $email = $row['email'];
-      $phone_number = $row['phone_number'];
-      $hire_date = $row['hire_date'];
-      $manager_id = $row['manager_id'];
-      $department_id = $row['department_id'];        
-	  $jobCode = $row['job_id'];
-	  $jobTitle = $row['job_title'];
-	  $salary = $row['salary'];
+  echo "<table>";
+  // second, for each matching job title, show the average salary
+  while ($row = mysqli_fetch_assoc($result)) {
+    $jobId = $row['job_id'];
+    $jobTitle = $row['job_title'];
+    $query2 = "select avg(salary),count(*) from employees where job_id = '$jobId';";
+    $result2 = mysqli_query($connect, $query2);
+    $row2 = mysqli_fetch_assoc($result2);
+    $averageSalary = $row2['avg(salary)'];
+    $count = $row2['count(*)'];
+    echo "<tr><th rowspan='$count' scope='row'>$jobTitle</th>";
 
-      // lookup department name
-      $query2 = "select department_name from departments where department_id = $department_id;";
-      $result2 = mysqli_query($connect, $query2);
-      if ($result2 && (mysqli_num_rows($result2) > 0)) {
-        $row2 = mysqli_fetch_assoc($result2);
-        $department = $row2['department_name'];    
-      } else {
-        $department = "error: " . mysqli_error();
-      }
-      
-      // lookup manager name
-      $query3 = "select first_name,last_name from employees where employee_id = $manager_id;";
-      $result3 = mysqli_query($connect, $query3);
-      if ($result3 && (mysqli_num_rows($result3) > 0)) {
-        $row3 = mysqli_fetch_assoc($result3);
-        $manager = $row3['last_name'];    
-      } else {
-        $manager = "error: " . mysqli_error();
-      }
-      if (!$manager_id) { $manager = "N/A"; }
-      echo "<tr><td>$empID</td><td>$firstName</td><td>$lastName</td><td>$email</td><td>$phone_number</td><td>$jobCode</td><td>$jobTitle</td><td>$" 
-           . number_format($salary, 2) . "</td><td>$hire_date</td><td>$department</td><td>$manager</td></tr>";
+    $query3 = "select * from employees where job_id = $jobId;";
+    $result3 = mysqli_query($connect, $query3);
+    $row3 = mysqli_fetch_assoc($result3);
+    $firstName = $row3['first_name'];
+    $lastName = $row3['last_name'];
+    $salary = $row3['salary'];
+    echo "<td>$firstName</td><td>$lastName</td><td class='right'>$" . number_format($salary, 2) . "</td>";
+
+    echo "<td rowspan='$count' class='center'>Average Salary<br><b>$" . number_format($averageSalary, 2) . "</b></td></tr>";
+    //echo "<tr><th rowspan='$count' colspan='2'>$jobTitle</th><td>Average Salary: <b>$" . number_format($averageSalary, 2) . "</b></td></tr>";
+    // list the employees
+    //$query3 = "select * from employees where job_id = $jobId;";
+    //$result3 = mysqli_query($connect, $query3);
+    while ($row3 = mysqli_fetch_assoc($result3)) {
+      $firstName = $row3['first_name'];
+      $lastName = $row3['last_name'];
+      $salary = $row3['salary'];
+      echo "<tr><td>$firstName</td><td>$lastName</td><td class='right'>$" . number_format($salary, 2) . "</td></tr>";
     }
-    echo "</table>";
+      
+  }
+  echo "</table>";
 }
-
-
 mysqli_close($connect);
 ?>
 </main>
 <footer>
-<a href="group4-hr-job-title.html">Return to Employee Search</a>
+<a href="group4-hr-job-title.html">Return to Job Search</a>
 </footer>
+</div>
 </body>
 </html>
 
