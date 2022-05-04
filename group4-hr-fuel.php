8041c1d2c50fbf7a1831ee432ab3730db9dc9f63
kristophpreston <90706024+kristophpreston@users.noreply.github.com>
2022-05-04 18:57:59 -0400

    Add files via upload
    Updated

--- group4-hr-fuel.php
+++ group4-hr-fuel.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-		<!--Name:
+	<!--Name:
 				filename: group4-hr-fuel.php
 				Blackboard Username: zrhines
 				Class Section: CTI.110.0004
@@ -11,44 +12,57 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Total Fuel Cost</title>
 	<link href="group4-hr-style.css" type="text/css" rel="stylesheet" />
-</head>
-<body>
-<div class="container">
-	<header>
-		<h1>Total Fuel Cost</h1>
-	</header>
-<main>
-<?php
-# The following function is called whenever your program needs to input data.
-# You can change it to use other sources rather than fgets().
-function input() {
-    return fgets(STDIN);
-}
 
-$avgMPG = $_POST['avgMPG'];
-$exptCost = $_POST['exptCost'];
-$tripLength = $_POST['tripLength'];
-if ($avgMPG >= 0) {
-    if ($exptCost >= 0) {
-        if ($tripLength >= 0) {
-            $travelCost = $tripLength / $avgMPG * $exptCost;
-            echo "Your total travel cost is: $travelCost" . PHP_EOL;
-        } else {
-            echo "Value cannot be below zero." . PHP_EOL;
-        }
-    } else {
-        echo "Value cannot be below zero." . PHP_EOL;
-    }
-} else {
-    echo "Value cannot be below zero." . PHP_EOL;
-}
-?>
-</main>
-<footer>
-	<br>
-	<br>
-<a href="group4-hr-fuel.html">Return to Fuel Reimbursement Calculation</a>
-</footer>
-</body>
+	<style>
+		p {
+			text-align: center;
+			font-size: large;
+			font-weight: 100;
+		}
+	</style>
+
+</head>
+<div class="container">
+
+	<body>
+		<header>
+			<h1>Total Fuel Cost</h1>
+		</header>
+		<main id="fuel">
+			<?php
+			# The following function is called whenever your program needs to input data.
+			# You can change it to use other sources rather than fgets().
+			function input()
+			{
+				return fgets(STDIN);
+			}
+
+			$avgMPG = $_POST['avgMPG'];
+			$exptCost = $_POST['exptCost'];
+			$tripLength = $_POST['tripLength'];
+			if ($avgMPG >= 0) {
+				if ($exptCost >= 0) {
+					if ($tripLength >= 0) {
+						$travelCost = $tripLength / $avgMPG * $exptCost;
+						$travelCost = round($travelCost, 2);
+						print ("<p>Your total travel cost is: $$travelCost.</p>") . PHP_EOL;
+					} else {
+						echo "Value cannot be below zero." . PHP_EOL;
+					}
+				} else {
+					echo "Value cannot be below zero." . PHP_EOL;
+				}
+			} else {
+				echo "Value cannot be below zero." . PHP_EOL;
+			}
+			?>
+		</main>
+		<footer>
+			<br>
+			<br>
+			<a href="group4-hr-fuel.html">Return to Fuel Reimbursement Calculation</a>
+		</footer>
+	</body>
 </div>
+
 </html>
